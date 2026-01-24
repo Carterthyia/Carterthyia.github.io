add61138306ccb40bb30c6645b50b42c3b711e89
卡提希娅 <1959516061@qq.com>
2026-01-24 22:31:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// ================= 全局变量 =================
-const siteConfig = {
-  adminAccount: "admin",
-  adminPassword: "carterthyia",
-  siteName: "卡提希娅的星光小站",
-  siteDomain: "www.carterthyia.com",
-  siteDescription: "于光影中守护的温柔使者 · 双形态战斗达人卡提希娅专属站点",
-  defaultTheme: "purple",
-  defaultMode: "light",
-  videoConfig: {
-    smallCardVideo: "videos/111.mp4",
-    bigCardVideo: "videos/12.mp4",
-    videoPoster1: "1.jpg",
-    videoPoster2: "2.jpg"
-  },
-  galleryCount: 8,
-  galleryTypes: ["all", "art", "battle", "daily", "beauty"]
-};
-window.siteConfig = siteConfig;
+
 
 let currentTheme = window.siteConfig.defaultTheme;
 let isDarkMode = window.siteConfig.defaultMode === 'dark';
